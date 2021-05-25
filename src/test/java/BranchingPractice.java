9ed98d9151c5b1d4d661df5e50508c974c844c54
raif pasha <rpasha3@hotmail.com>
2021-05-25 19:23:33 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,11 @@
 public class BranchingPractice {
     public static void main(String[] args) {
 
+        // created new project
+        // enabled version control
+        // added .gitignore with .idea , target folder, and .iml file
+        // made initial commit by keeping track of all files
+        // shared project on github
     }
 
 }
